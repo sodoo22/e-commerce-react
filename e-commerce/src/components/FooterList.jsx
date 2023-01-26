ff039ee50438c27a8c9a263f0224b76456f2ead9
Sodoo22 <sodoo75@gmail.com>
2023-01-26 13:23:20 +0800

    cl

--- e-commerce/src/components/FooterList.jsx
+++ e-commerce/src/components/FooterList.jsx
@@ -1,22 +1,18 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 export default function FooterList(props) {
-    // const [list, setList] = useState()
-    // console.log(FindData)
-    return (
-        <div className="footerList">
-            <h3>Find product</h3>
-            <ul>
-                {props.FindData.map((data, index) => {
-                    return (
-                        <li key={index} className="my-3">
-                            <Link >{data.ul}</Link>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+  return (
+    <div className="footerList">
+      <h3>Find product</h3>
+      <ul>
+        {props.data.map((data, index) => {
+          return (
+            <li key={index} className="my-3">
+              <Link>{data.ul}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
--- e-commerce/src/data/footer.js
+++ e-commerce/src/data/footer.js
@@ -1,72 +1,71 @@
 const FindData = [
-    {
-        id: 1,
-        ul: "Brown Arnold",
-    },
-    {
-        id: 2,
-        ul: "Chronograph blue",
-    },
-    {
-        id: 3,
-        ul: "Smart Phones",
-    },
-    {
-        id: 4,
-        ul: "Smart Watch",
-    },
-    {
-        id: 5,
-        ul: "Smart TVs",
-    },
+  {
+    id: 1,
+    ul: "Brown Arnold",
+  },
+  {
+    id: 2,
+    ul: "Chronograph blue",
+  },
+  {
+    id: 3,
+    ul: "Smart Phones",
+  },
+  {
+    id: 4,
+    ul: "Smart Watch",
+  },
+  {
+    id: 5,
+    ul: "Smart TVs",
+  },
 ];
 
 const GetData = [
-    {
-        id: 1,
-        ul: "Brown Arnold",
-    },
-    {
-        id: 2,
-        ul: "Brown 2",
-    },
-    {
-        id: 3,
-        ul: "Brown 3",
-    },
-    {
-        id: 4,
-        ul: "Brown 4",
-    },
-    {
-        id: 5,
-        ul: "Brown 5",
-    },
+  {
+    id: 1,
+    ul: "About us",
+  },
+  {
+    id: 2,
+    ul: "Contact us",
+  },
+  {
+    id: 3,
+    ul: "Return policy",
+  },
+  {
+    id: 4,
+    ul: "Privacy policy",
+  },
+  {
+    id: 5,
+    ul: "Payment policy",
+  },
 ];
 
 const AboutData = [
-    {
-        id: 1,
-        ul: "Brown Arnold",
-    },
-    {
-        id: 2,
-        ul: "Brown 2",
-    },
-    {
-        id: 3,
-        ul: "Brown 3",
-    },
-    {
-        id: 4,
-        ul: "Brown 4",
-    },
-    {
-        id: 5,
-        ul: "Brown 5",
-    },
+  {
+    id: 1,
+    ul: "News",
+  },
+  {
+    id: 2,
+    ul: "Service",
+  },
+  {
+    id: 3,
+    ul: "Our policy",
+  },
+  {
+    id: 4,
+    ul: "Customer car",
+  },
+  {
+    id: 5,
+    ul: "FAQ's",
+  },
 ];
 
-export default FindData;
-// export default { FindData, GetData, AboutData };
-
+// export default FindData;
+export { FindData, GetData, AboutData };
